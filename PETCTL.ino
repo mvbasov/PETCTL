724a1899a3234d90c72253e78df260fa7c04291f
Mikhail Basov <lineageos@basov.net>
2021-01-22 23:02:54 +0300

    Use Kalman (simple) filter for temperature.

--- PETCTL.ino
+++ PETCTL.ino
@@ -270,5 +270,20 @@ float getTemp() {
   steinhart = 1.0 / steinhart;                 // Invert
   steinhart -= 273.15;                         // convert absolute temp to C
 
-  return steinhart;
+  return simpleKalman(steinhart);
+}
+
+// Простой “Калман” 
+// https://alexgyver.ru/lessons/filters/
+float _err_measure = 0.8;  // примерный шум измерений
+float _q = 0.1;   // скорость изменения значений 0.001-1, варьировать самому
+float simpleKalman(float newVal) {
+  float _kalman_gain, _current_estimate;
+  static float _err_estimate = _err_measure;
+  static float _last_estimate;
+  _kalman_gain = (float)_err_estimate / (_err_estimate + _err_measure);
+  _current_estimate = _last_estimate + (float)_kalman_gain * (newVal - _last_estimate);
+  _err_estimate =  (1.0 - _kalman_gain) * _err_estimate + fabs(_last_estimate - _current_estimate) * _q;
+  _last_estimate = _current_estimate;
+  return _current_estimate;
 }

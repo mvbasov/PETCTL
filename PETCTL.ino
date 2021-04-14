bc498d97018a504bf41401cf17074a92259d57a7
Mikhail Basov <lineageos@basov.net>
2021-04-15 00:28:40 +0300

    Add config section

--- PETCTL.ino
+++ PETCTL.ino
@@ -1,65 +1,116 @@
 //#define SERIAL_DEBUG
-/*
-Reductor constant ~ 4.69624E-6 m/deg (length to stepper rotation degree)
- ((36/8) * (36/8) * (55/8)) = 139.21875
- 139.21875 - gear ratio for RobertSa reductor
- ((36/8) * (55/8)) = 30.9375
- 30.9375 - gear ratio for Zneipas reductor
-Bobin round length
- 74 * Pi = 232.478
- 232.478 mm - bobin round length
-*/
-const float REDCONST = 232.478 /(360 * 139.21875 * 1000);
+/* Stepper driver microstep devision */
+#define CFG_STEP_DIV 4
+/* Which pin stepper driver STEP pin connected */
+#define CFG_STEP_STEP_PIN 6
+/* Which pin stepper driver DIR pin connected */
+#define CFG_STEP_DIR_PIN 5
+/* Which pin stepper driver EN pin connected */
+#define CFG_STEP_EN_PIN 1
+#define CFG_ENC_CLK 3
+#define CFG_ENC_DT 2
+#define CFG_ENC_SW 4
+#define CFG_TEMP_INIT 180
+#define CFG_TERM_PIN A0
+#define CFG_TERM_VALUE 100000
+#define CFG_TERM_VALUE_TEMP 25
+#define CFG_TERM_B_COEFF 4388
+#define CFG_TERM_SERIAL_R 4700
+#define CFG_ENDSTOP_PIN 8
+#define CFG_PULL_EXTRA_LENGTH 0.15
+#define CFG_PID_P 14
+#define CFG_PID_I 0.93
+#define CFG_PID_D 59.87
+#define CFG_HEATER_PIN 9
+#define CFG_RED_G1 36/8
+#define CFG_RED_G2 36/8
+#define CFG_RED_G3 55/8
+#define CFG_BOBIN_DIAM 74
+#define CFG_SPEED_INIT 2.5
 
+/*
+  NOTHING TO CONFIGURE AFTER THIS LINE
+*/
 
 #include "GyverStepper.h"
-int stepDiv = 4; // DRV8825: 1/4 MS0=0, MS1=1, MS2=0
+//int stepDiv = 4; // DRV8825: 1/4 MS0=0, MS1=1, MS2=0
 // 6 - STEP
 // 5 - DIR
 // 1 - EN
-GStepper<STEPPER2WIRE> stepper(200 * stepDiv, 6, 5, 1);
+//GStepper<STEPPER2WIRE> stepper(200 * stepDiv, 6, 5, 1);
+GStepper<STEPPER2WIRE> stepper(200 * CFG_STEP_DIV, CFG_STEP_STEP_PIN, CFG_STEP_DIR_PIN, CFG_STEP_EN_PIN);
 
 #include "GyverTimers.h"
 
 #include "GyverOLED.h"
 GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
 
-#define CLK 3
-#define DT 2
-#define SW 4
+//#define CLK 3
+//#define DT 2
+//#define SW 4
+#define CLK CFG_ENC_CLK
+#define DT CFG_ENC_DT
+#define SW CFG_ENC_SW
 #include "GyverEncoder.h"
 Encoder enc1(CLK, DT, SW);
 int value = 0;
 
 // Termistor definition
 float prevTemp, curTemp = 0;
-float targetTemp = 180;
+//float targetTemp = 180;
+float targetTemp = CFG_TEMP_INIT;
 
 // which analog pin to connect
-#define THERMISTORPIN A0         
+//#define THERMISTORPIN A0         
+#define THERMISTORPIN CFG_TERM_PIN         
 // resistance at 25 degrees C
+//#define THERMISTORNOMINAL 100000      
 #define THERMISTORNOMINAL 100000      
 // temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25   
+//#define TEMPERATURENOMINAL 25   
+#define TEMPERATURENOMINAL CFG_TERM_VALUE_TEMP   
 // The beta coefficient of the thermistor (usually 3000-4000)
-#define BCOEFFICIENT 4388
+//#define BCOEFFICIENT 4388
+#define BCOEFFICIENT CFG_TERM_B_COEFF
 // the value of the 'other' resistor
-#define SERIESRESISTOR 4700    
+//#define SERIESRESISTOR 4700    
+#define SERIESRESISTOR CFG_TERM_SERIAL_R    
 
 // End stop pin
-#define ENDSTOP 8
+//#define ENDSTOP 8
+#define ENDSTOP CFG_ENDSTOP_PIN
 // Extra length to pull after end stop triggered (in m)
-#define EXTRA_LENGTH 0.07
+//#define EXTRA_LENGTH 0.07
+#define EXTRA_LENGTH CFG_PULL_EXTRA_LENGTH
 float finalLength = 0;
 
 #include "GyverPID.h"
-GyverPID regulator(14, 0.93, 59.87, 200);
+//GyverPID regulator(14, 0.93, 59.87, 200);
+GyverPID regulator(CFG_PID_P, CFG_PID_I, CFG_PID_D, 200);
 
-boolean runMotor=false;
-long Speed = 2.5/(REDCONST * 1000); // 539 degree/sec for 2.5 mm/s speed
-
-#define HEATER_PIN 9
+//#define HEATER_PIN 9
+#define HEATER_PIN CFG_HEATER_PIN
 boolean Heat = false;
+
+/*
+Reductor constant ~ 4.69624E-6 m/deg (length to stepper rotation degree)
+ ((36/8) * (36/8) * (55/8)) = 139.21875
+ 139.21875 - gear ratio for RobertSa reductor
+ ((36/8) * (55/8)) = 30.9375
+ 30.9375 - gear ratio for Zneipas reductor
+*/
+#define GEAR_RATIO CFG_RED_G1 * CFG_RED_G2 * CFG_RED_G3
+/*
+Bobin round length
+ 74 * Pi = 232.478
+ 232.478 mm - bobin round length
+*/
+#define BOBIN_ROUND_LENGTH 3.1415926 * CFG_BOBIN_DIAM
+//const float REDCONST = 232.478 /(360 * 139.21875 * 1000);
+const float REDCONST = BOBIN_ROUND_LENGTH /(360 * GEAR_RATIO * 1000);
+boolean runMotor=false;
+//long Speed = 2.5/(REDCONST * 1000); // 539 degree/sec for 2.5 mm/s speed
+long Speed = CFG_SPEED_INIT/(REDCONST * 1000); // 539 degree/sec for 2.5 mm/s speed
 
 #define CHANGE_NO 0
 #define CHANGE_TEMPERATURE 1
